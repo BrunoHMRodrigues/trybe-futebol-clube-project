e1f72b8bd6b6e0cc12f01dc67f325601d6c6ac43
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2023-06-14 11:13:43 -0300

    Feature: Requisite 01 - models, migrations and interfaces for teams, matches and users created

--- app/backend/src/database/models/MatchModel.ts
+++ app/backend/src/database/models/MatchModel.ts
@@ -5,7 +5,6 @@ import {
   InferCreationAttributes,
 } from 'sequelize';
 import db from '.';
-// import OtherModel from './OtherModel';
 
 class MatchModel extends Model<InferAttributes<MatchModel>,
 InferCreationAttributes<MatchModel>> {
@@ -58,16 +57,5 @@ MatchModel.init({
   timestamps: false,
   underscored: true,
 });
-
-/**
-  * `Workaround` para aplicar as associations em TS:
-  * Associations 1:N devem ficar em uma das instâncias de modelo
-  * */
-
-// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
-// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
-
-// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
-// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
 
 export default MatchModel;
